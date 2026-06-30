2f975696e066178c4796c497ae81ee55bc0c56e9
FoxySonne <meeeeeeezzz@gmail.com>
2026-06-30 04:26:23 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,21 @@
+// Загрузка любого HTML-файла в нужное место страницы
+function loadBlock(containerId, filePath) {
 
+    const container = document.getElementById(containerId);
+
+    if (!container) return;
+
+    fetch(filePath)
+        .then(response => response.text())
+        .then(html => {
+            container.innerHTML = html;
+        });
+
+}
+
+// Загрузка страницы в центральную область
+function loadPage(pageName) {
+
+    loadBlock("page-content", "pages/" + pageName);
+
+}
