d85b096c9212e8dc718eeee0580aced73957eb14
FoxySonne <meeeeeeezzz@gmail.com>
2026-06-30 17:54:52 +0300

    Create database.js

---
+++ data/database.js
@@ -0,0 +1,66 @@
+{
+  "categories": [
+    { "name": "Снаряжение", "id": "equipment" },
+{ "name": "Герои", "id": "heroes" },
+{ "name": "Титан", "id": "titan" },
+{ "name": "Ускорения", "id": "speedups" },
+{ "name": "Увеличьте силу на 1", "id": "power" },
+{ "name": "Коллекция шефа", "id": "chief_collection" },
+{ "name": "Сбор ресурсов", "id": "resource_gathering" },
+{ "name": "Улучшение войск", "id": "troop_upgrade" },
+{ "name": "Прочее", "id": "misc" },
+}
+
+actions: {
+{ name: "Синий болт", id: "blue_bolt, categoryId: "equipment" },
+{ name: " Фиолетовый болт ", id: " purple_bolt ", categoryId: "equipment"},
+=
+{ name: " Золотой болт ", id: " gold_bolt ", categoryId: "equipment"},
+{ name: "? болт ", id: " unknown_bolt ", categoryId: "equipment"},
+{ name: " Увеличение звезд снаряжения ", id: " equipment_stars ", categoryId: "equipment"},
+{ name: " Молот перековки ", id: " reforge_hammer ", categoryId: "equipment"},
+{ name: " Изолента ", id: " insulation_tape ", categoryId: "equipment"},
+
+{ name: " Желаемый призыв ", id: " wish_summon ", categoryId: "heroes"},
+{ name: " Продвинутый призыв ", id: " advanced_summon ", categoryId: "heroes"},
+{ name: " Стандартный призыв ", id: " standard_summon ", categoryId: "heroes"},
+{ name: " Синий фрагмент героя ", id: " blue_hero_card ", categoryId: "heroes"},
+{ name: " Фиолетовый фрагмент героя ", id: " purple_hero_card ", categoryId: "heroes"},
+{ name: " Золотой фрагмент героя ", id: " gold_hero_card ", categoryId: "heroes"},
+{ name: " Книги героев ", id: " hero_books ", categoryId: "heroes"},
+
+{ name: " Опыт титана 100 ед ", id: " titan_exp_100", categoryId: "titan"},
+{ name: " Клетки титана ", id: " titan_cells ", categoryId: "titan"},
+{ name: "1 ед. биогенного белка ", id: " biogenic_protein ", categoryId: "titan"}, 
+
+{ name: " Ускорение исследований 1 мин ", id: " research_1m ", categoryId: "speedups"},
+{ name: " Ускорение строительства 1 мин ", id: " building_1m ", categoryId: "speedups"},
+{ name: " Ускорение исцеления 1 мин ", id: " healing_1m ", categoryId: "speedups"},
+{ name: " Ускорение обучения 1 мин ", id: " training_1m ", categoryId: "speedups"},
+
+{ name: " Увеличьте силу на 1 с помощью строительства ", id: " building_power ", categoryId: "power"},
+{ name: " Увеличьте силу на 1 с помощью исследования ", id: " research_power ", categoryId: "power"},
+
+{ name: " Чертежи ", id: blueprints "", categoryId: "chief_collection"},
+{ name: " Шестеренки ", id: " gears ", categoryId: "chief_collection"},
+
+{ name: " Собрать 100 ед. еды ", id: " food_100", categoryId: "resource_gathering"},
+{ name: " Собрать 100 ед. древесины ", id: " wood_100", categoryId: "resource_gathering"},
+{ name: " Собрать 20 ед. металла ", id: " metal_20", categoryId: "resource_gathering"},
+{ name: " Собрать 5 ед. топлива ", id: " fuel_5", categoryId: "resource_gathering"},
+
+{ name: " Покупки ", id: " purchases ", categoryId: "misc"},
+{ name: " Зараженные 1–6 ур ", id: " infected_1_6", categoryId: "misc"},
+{ name: " Зараженные 7–12 ур ", id: " infected_7_12", categoryId: "misc"},
+{ name: " Зараженные 13–18 ур ", id: " infected_13_18", categoryId: "misc"},
+{ name: " Зараженные 19–24 ур ", id: " infected_19_24", categoryId: "misc"},
+{ name: " Зараженные 25–30 ур ", id: " infected_25_30", categoryId: "misc"},
+{ name: "1 рейд на нечестивого зараженного ", id: " unholy_raid ", categoryId: "misc"},
+{ name: " Зараженный в разведмиссиях ", id: " infected_recon ", categoryId: "misc"},
+{ name: " Завершение разведмиссий пн, пт ", id: " recon_mon_fri ", categoryId: "misc"},
+{ name: " Завершение разведмиссий ср ", id: " recon_wed ", categoryId: "misc"},
+{ name: " Отправки S-класс ", id: " dispatch_s ", categoryId: "misc"},
+{ name: " Грузовики А-класс ", id: " truck_a ", categoryId: "misc"},
+{ name: " Используйте очки выносливости ", id: " stamina_points ", categoryId: "misc"},
+{ name: " Используйте очки VS ", id: " vs_points ", categoryId: "misc"},
+}
