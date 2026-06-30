33a7d2f24bac25b22a0eb4457ce74a4e05fb27c6
FoxySonne <meeeeeeezzz@gmail.com>
2026-06-30 04:41:29 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,16 @@
 // После полной загрузки страницы
 window.addEventListener("DOMContentLoaded", () => {
 
-    // Левое меню
     loadBlock(
         "sidebar-container",
         "components/sidebar.html"
     );
 
-    // Правая колонка
     loadBlock(
         "rightbar-container",
         "components/rightbar.html"
     );
 
-    // Главная страница
-    loadPage(
-        "pages/home.html"
-    );
+    loadPage("home.html");
 
 });
