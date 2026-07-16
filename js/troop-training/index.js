e5412e098338ce080c19f3921db5d6122b5ad273
FoxySonne <meeeeeeezzz@gmail.com>
2026-07-16 22:51:56 +0300

    Refresh troop training result renderer

--- js/troop-training/index.js
+++ js/troop-training/index.js
@@ -1,6 +1,6 @@
 import { getAdvancedMode, getElement } from "./dom.js";
 import { formatAvailableTimeInput, formatStageTimeInput } from "./format.js";
-import { applyStagePreset, initStages, renderResults, syncStageEnabledState } from "./render.js?v=20260716-1";
+import { applyStagePreset, initStages, renderResults, syncStageEnabledState } from "./render.js?v=20260716-2";
 import { getPreferredDay, getTransferStatusHtml, getTransferTargets, saveTransferPayload, saveTurboVsTransfer } from "./storage-transfer.js";
 
 function bindUnitToggles() {
