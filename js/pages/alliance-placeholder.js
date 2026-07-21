67497cb906aea56faee1d62c2af379393e5c123e
FoxySonne <meeeeeeezzz@gmail.com>
2026-07-21 19:05:45 +0300

    Route reservoir activity to its module

--- js/pages/alliance-placeholder.js
+++ js/pages/alliance-placeholder.js
@@ -1,6 +1,12 @@
 import { loadAlliancePageContext, fillAllianceCompactHeader } from "../alliance/page-context.js?v=20260718-1";
 
 export async function init() {
+  const currentPage = localStorage.getItem("currentPage") || "";
+  if (currentPage.endsWith("alliance/reservoir-activity.html")) {
+    const module = await import("./alliance-reservoir-activity.js?v=20260721-1");
+    return module.init();
+  }
+
   const box = document.getElementById("allianceMessage");
   try {
     const context = await loadAlliancePageContext(window.harvestHubSupabase);
