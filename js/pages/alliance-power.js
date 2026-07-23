fbf8f14084768b318d9329c061725a59296802a2
FoxySonne <meeeeeeezzz@gmail.com>
2026-07-23 22:51:52 +0300

    Restrict power page to alliance editors

--- js/pages/alliance-power.js
+++ js/pages/alliance-power.js
@@ -1,10 +1,14 @@
-import { initPowerSection } from "../alliance/power-section.js?v=20260721-1";
-import { loadAlliancePageContext, fillAllianceCompactHeader } from "../alliance/page-context.js?v=20260718-1";
+import { initPowerSection } from "../alliance/power-section.js?v=20260723-editing-1";
+import { loadAlliancePageContext, fillAllianceCompactHeader, canEditAlliance } from "../alliance/page-context.js?v=20260718-1";
 
 export async function init() {
   const box = document.getElementById("allianceMessage");
   try {
     const context = await loadAlliancePageContext(window.harvestHubSupabase);
+    if (!canEditAlliance(context)) {
+      window.loadPage?.("alliance/members.html");
+      return;
+    }
     fillAllianceCompactHeader(context);
     initPowerSection();
   } catch (error) {
