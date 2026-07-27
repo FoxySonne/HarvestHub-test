3f4ef623b618a8f53987f779667609827af409d1
FoxySonne <297433626+FoxySonne@users.noreply.github.com>
2026-07-28 01:16:47 +0300

    Обновить версии файлов периодов членства

--- js/core/navigation.js
+++ js/core/navigation.js
@@ -1,7 +1,7 @@
 (() => {
-  const SITE_ASSET_VERSION = "20260728-power-row-editor-1";
-  const DEFAULT_STYLESHEET = "css/style.css?v=20260728-power-row-editor-1";
-  const VS_STYLESHEET = "css/style-vs-main.css?v=20260727-main-vs-1";
+  const SITE_ASSET_VERSION = "20260728-membership-periods-1";
+  const DEFAULT_STYLESHEET = "css/style.css?v=20260728-membership-periods-1";
+  const VS_STYLESHEET = "css/style-vs-main.css?v=20260728-membership-periods-1";
   const VS_PAGE = "alliance/vs.html";
   const QUICK_LINKS_STORAGE_KEY = "harvesthub_page_visits";
   const MAX_QUICK_LINKS = 5;
